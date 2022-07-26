5cd2bad42d33f41691cccae55c6a9241d9942329
saul <saul.yeponde@gmail.com>
2022-07-26 20:02:44 +0200

    Player lap authenticity
    Added a mechanic to prevent player from crossing the finish back anf forth.
    Added checkpoints on the track.
    Added a checpoint collider.
    Added a new tag for the checpoints.

--- Assets/Scripts/Constants/TagsConstants.cs
+++ Assets/Scripts/Constants/TagsConstants.cs
@@ -6,4 +6,5 @@ public class TagsConstants
     public static string LAP_MANAGER_TAG = "LapManager";
     public static string DAMAGING_OBSTACLE_TAG = "DamagingObstacle";
     public static string SLOWING_OBSTACLE_TAG = "SlowingObstacle";
+    public static string CHECKPOINT_TAG = "Checkpoint";
 }
--- Assets/Scripts/Managers/LapManager.cs
+++ Assets/Scripts/Managers/LapManager.cs
@@ -8,20 +8,53 @@ public class LapManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI lapCounterText;
     private int lapCounter = 0;
+    private int currentCheckpointIndex = 0;
+    private int targetCheckpointsCount = 1;
+
+    private void Start()
+    {
+        GameObject[] checkpointObjects = GameObject.FindGameObjectsWithTag(TagsConstants.CHECKPOINT_TAG);
+        this.targetCheckpointsCount = checkpointObjects.Length;
+    }
 
     private void OnEnable()
     {
         LapCollider.OnLapFinished += UpdateLap;
+        LapCollider.OnLapFinished += ResetCheckpointIndex;
+        LapCollider.OnLapAuthenticityCheckOk += IsAllCheckpointsValidated;
+        CheckpointCollider.OnCheckpointEnter += ValidateCheckpoint;
     }
 
     private void OnDisable()
     {
         LapCollider.OnLapFinished -= UpdateLap;
+        LapCollider.OnLapFinished -= ResetCheckpointIndex;
+        LapCollider.OnLapAuthenticityCheckOk -= IsAllCheckpointsValidated;
+        CheckpointCollider.OnCheckpointEnter -= ValidateCheckpoint;
     }
+
     public void UpdateLap()
     {
         this.LapCounter++;
         this.lapCounterText.text = $"Laps: {this.LapCounter}";
+    }
+
+    public void ValidateCheckpoint(int checkpointIndexTovalidate)
+    {
+        if(this.currentCheckpointIndex == checkpointIndexTovalidate - 1)
+        {
+            this.currentCheckpointIndex = checkpointIndexTovalidate;
+        }
+    }
+
+    public bool IsAllCheckpointsValidated()
+    {
+        return currentCheckpointIndex == targetCheckpointsCount;
+    }
+
+    public void ResetCheckpointIndex()
+    {
+        this.currentCheckpointIndex = 0;
     }
 
     public int LapCounter { get => lapCounter; set => lapCounter = value; }
