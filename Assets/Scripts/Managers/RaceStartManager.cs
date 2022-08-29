2643b90165e1b3545f41653c184c2e3c75a603a7
saul <saul.yeponde@gmail.com>
2022-08-29 23:09:41 +0200

    Ui implementation
    Added the trafic ligth sprites.
    Added Scripts to manage the race start properly.

---
+++ Assets/Scripts/Managers/RaceStartManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RaceStartManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI starterText;
+    private int count = 3;
+
+
+    private void OnEnable()
+    {
+        StarterLightManager.OnCountDownTrigger += StartCountDown;
+    }
+
+    private void OnDisable()
+    {
+        StarterLightManager.OnCountDownTrigger -= StartCountDown;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void StartCountDown()
+    {
+        starterText.gameObject.SetActive(true);
+        if(count > 0)
+        {
+            starterText.text = count.ToString();
+            count--;
+        } else
+        {
+            starterText.text = "GO!!";
+            count = 3;
+        }
+        
+    }
+}
